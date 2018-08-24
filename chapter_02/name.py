cf802cc1ba13bf5710bf0d745900c282af2a0bef
xiaominglei001 <xiaominglei001@qq.com>
2018-08-24 22:01:07 +0800

    Update name.py

--- chapter_02/name.py
+++ chapter_02/name.py
@@ -2,5 +2,7 @@ first_name = "ada"
 last_name = "lovelace"
 full_name = first_name + " " + last_name
 
-message = "Hello, " + full_name.title() + "!" #full_name.title()：以首字母大写的方式来显示单词 full_name.upper()：全改为大写 full_name.lower()：全改为小写。
+message = "Hello, " + full_name.title() + "!" 
+#full_name.title()：以首字母大写的方式来显示单词 full_name.upper()：全改为大写 full_name.lower()：全改为小写。
+#full_name.strip() 剔除字符串前后两端空白。
 print(message)
