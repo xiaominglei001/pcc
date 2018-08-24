1d641a70309904999c4a6208368571a9a0672861
xiaominglei001 <xiaominglei001@qq.com>
2018-08-24 21:54:01 +0800

    Update name.py

--- chapter_02/name.py
+++ chapter_02/name.py
@@ -2,5 +2,5 @@ first_name = "ada"
 last_name = "lovelace"
 full_name = first_name + " " + last_name
 
-message = "Hello, " + full_name.title() + "!"
+message = "Hello, " + full_name.title() + "!" #full_name.title()：以首字母大写的方式来显示单词 full_name.upper()：全改为大写 full_name.lower()：全改为小写。
 print(message)
